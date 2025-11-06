42431379ae34fb983703ef0e6e0db3133df57157
Junyou Park <pjy008008@gmail.com>
2025-11-06 19:07:47 +0900

    feat(config): Spring Security 설정 및 H2 데이터베이스 콘솔 접속 허가
    개발 환경에서 H2 데이터베이스 콘솔에 접근할 수 있도록 Spring Security 설정을 추가합니다.
    - `/h2-console/**` 경로에 대해 `permitAll` 설정
    - H2 콘솔 접근을 위해 CSRF 보호 비활성화

---
+++ src/main/java/com/pjy008008/j_community/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.pjy008008.j_community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .formLogin(formLogin -> formLogin.defaultSuccessUrl("/", true))
+                .httpBasic(withDefaults -> {
+                });
+
+        return http.build();
+    }
+}
