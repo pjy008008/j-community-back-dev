0349f68e693c98d727fb7bca8bd615a4abec0dff
Junyou Park <pjy008008@gmail.com>
2025-11-16 04:36:30 +0900

    feat(entity): User 엔티티 정의
    Spring Security의 UserDetails를 구현하는 User 엔티티 구현
    - `username`, `password`, `email` 필드 추가

---
+++ src/main/java/com/pjy008008/j_community/entity/User.java
@@ -0,0 +1,73 @@
+package com.pjy008008.j_community.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // 모든 사용자에게 "ROLE_USER" 권한 부여. 추후 필요하다면 Role 엔티티 추가 예정
+        return Collections.singletonList(() -> "ROLE_USER");
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
