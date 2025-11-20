ab5c0d9f3ce5546035b60b03fbbe96bfdae71fbf
Junyou Park <pjy008008@gmail.com>
2025-11-20 15:42:59 +0900

    feat(config): 커뮤니티 수정/삭제 API 보안 설정
    커뮤니티 수정, 삭제 API에 대한 접근 권한을 SecurityFilterChain에 설정

--- src/main/java/com/pjy008008/j_community/config/SecurityConfig.java
+++ src/main/java/com/pjy008008/j_community/config/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/posts").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/communities/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/communities").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/communities/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/communities/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/api/posts/{postId}/comments").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/posts/{postId}/comments").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/comments/{commentId}/replies").authenticated()
